f1b2da0ebdaffe666a90ab4d52282c3e294a597c
Pepe Ramos <jramma@MacBook-Air-de-Pepe.local>
2023-08-25 17:53:03 +0200

    translation added

---
+++ astro-i18next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('astro-i18next').AstroI18nextConfig} */
+export default {
+    defaultLocale: "en",
+    locales: ["en", "es"],
+    namespaces: ["index", "work", "about"],
+    defaultNamespace: "index",
+
+  };
